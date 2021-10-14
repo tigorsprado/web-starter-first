5f19d643845931b2425a7adf80827f279a5ec06f
Tiago Prado <tiago_sprado@hotmail.com>
2021-10-14 02:00:19 -0300

    Adicionado o git ignore

---
+++ script.js
@@ -0,0 +1,9 @@
+var nome = "André";
+var sobrenome = "Pereira";
+
+if (nome == "André" && sobrenome == "Pereira"){
+    console.log("Você é da família Pereira")
+}else {
+    console.log("Você não pertence a essa família")
+}
+
